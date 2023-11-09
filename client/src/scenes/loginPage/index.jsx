58169bf2b0828c1dbeacb31a2be14365e330e2ab
bobo <q463250938@gmail.com>
2023-11-08 22:42:21 -0800

    form functionality with formik & yup for form validation

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,43 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import MyForm from "./Form";
+
+
 const LoginPage = () => {
+    const theme = useTheme();
+    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+    const navigate = useNavigate();
+    const primaryLight = theme.palette.primary.light
+
     return (
-        <div>LoginPage</div>
+        <Box 
+            width="100%" 
+            backgroundColor={theme.palette.background.alt} 
+            p="1rem 6%" 
+            textAlign="center">
+            <Box>
+                <Typography
+                    fontWeight="bold"
+                    fontSize="32px"
+                    color="primary"
+                >
+                    Sociopedia
+                </Typography>
+            </Box>
+            <Box
+                width={isNonMobileScreen ? "50%" : "93%"}
+                p="2rem"
+                m="2rem auto"
+                borderRadius="1.5rem"
+                backgroundColor={theme.palette.background.alt}
+            >
+                <Typography>
+                    Welcome to Sociopedia, the Social Media for Sociopaths
+                </Typography>
+                <MyForm />
+                
+            </Box>
+        </Box>
     )
 }
 
