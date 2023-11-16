d16168db98041d949fa2e3ffdf57db5de291251d
bobo <q463250938@gmail.com>
2023-11-16 14:53:33 -0800

    finishing up the homepage design and fetching to the server for posts is successful

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -6,41 +6,42 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidgets";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 
-
-
 const HomePage = () => {
-    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
-    const { _id, picturePath }  = useSelector((state) => state.user);
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  const posts = useSelector((state) => state.posts);
 
-    return (
-        <>
-            <Navbar />
-            <Box
-                width="100%"
-                padding="2rem 6%"
-                display={isNonMobileScreen ? "flex" : "block"}
-                gap="0.6rem"
-                justifyContent="space-between"
+  return (
+    <>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreen ? "flex" : "block"}
+        gap="0.6rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
 
-            >
-                <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
-                    <UserWidget userId={_id} picturePath={picturePath}/>
-                </Box>
-
-                <Box 
-                    flexBasis={isNonMobileScreen ? "42%" : undefined}
-                    mt= {isNonMobileScreen ? undefined : "2rem"}
-                >
-                    <MyPostWidget picturePath={picturePath} />
-                </Box>
-                {isNonMobileScreen && (
-                    <Box flexBasis="26%">
-                    </Box>
-                )}
-
-            </Box>
-        </>
-    )
-}
+        <Box
+          flexBasis={isNonMobileScreen ? "42%" : undefined}
+          mt={isNonMobileScreen ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          {
+            <ul>
+              {posts.map((post, index) => {
+                return <li>{post.description}</li>;
+              })}
+            </ul>
+          }
+        </Box>
+        {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+      </Box>
+    </>
+  );
+};
 
 export default HomePage;
