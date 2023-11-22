db35675373eeaa1a77f8fc0ed32f5e9b94446da1
bobo <q463250938@gmail.com>
2023-11-21 22:24:14 -0800

    adding voice upload feature inc

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
