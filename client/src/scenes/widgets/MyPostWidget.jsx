df681e5ef93d700bfc9585b09fb7fdc6f445e363
bobo <q463250938@gmail.com>
2023-11-22 21:30:28 -0800

    toggle between audio / pictures under maintenance

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -31,6 +31,7 @@ import { setPost, setPosts } from "state";
 const MyPostWidget = ({ picturePath }) => {
   const dispatch = useDispatch();
   const [isImage, setIsImage] = useState(false);
+  const [isAudio, setIsAudio] = useState(false);
   const [image, setImage] = useState(null);
   const [post, setPost] = useState("");
   const { palette } = useTheme();
@@ -128,10 +129,61 @@ const MyPostWidget = ({ picturePath }) => {
         </Box>
       )}
 
+      {isAudio && (
+        <Box
+          border={`1px solid ${medium}`}
+          borderRadius="5px"
+          mt="1rem"
+          p="1rem"
+        >
+          <Dropzone
+            acceptFiles=".jpeg,.jpg,.png"
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])
+            }
+          >
+            {({ getRootProps, getInputProps }) => (
+              <FlexBetween>
+              <Box
+                {...getRootProps()}
+                border={`2px dashed ${palette.primary.main}`}
+                p="1rem"
+                width="100%"
+                sx={{ "&:hover": { cursor: "pointer" } }}
+              >
+                <input {...getInputProps()} />
+                {!image ? (
+                  <p>Add Audio Here</p>
+                ) : (
+                  <FlexBetween>
+                    <Typography>{image.name}</Typography>
+                    <EditOutlined />
+                  </FlexBetween>
+                )}
+              </Box>
+              {image && (
+                <IconButton
+                  onClick={() => setImage(null)}
+                  sx={{
+                    width: "15%"
+                  }}
+                >
+                  <DeleteOutlined />
+                </IconButton>
+              )}
+              </FlexBetween>
+            )}
+          </Dropzone>
+        </Box>
+      )}  
+
       <Divider sx={{ margin: "1.25rem 0"}}/>
 
       <FlexBetween>
-        <FlexBetween gap="0.25rem" onClick={() => setIsImage(!isImage)}>
+        <FlexBetween gap="0.25rem" onClick={() =>{ 
+            if (!isImage) setIsAudio(false);
+            setIsImage(!isImage)
+            }}>
           <ImageOutlined sx={{ color: mediumMain }}/> 
           <Typography
             color={mediumMain}
@@ -152,9 +204,15 @@ const MyPostWidget = ({ picturePath }) => {
               <Typography color={mediumMain}>Attachment</Typography>
             </FlexBetween>
 
-            <FlexBetween gap="0.25rem">
+            <FlexBetween gap="0.25rem" onClick={() => {
+                if (!isAudio) setIsImage(false);
+                setIsAudio(!isAudio);
+                }}>
               <MicOutlined sx={{color: mediumMain}} />
-              <Typography color={mediumMain}>Audio</Typography>
+              <Typography 
+                color={mediumMain}
+                sx={{ "&:hover": { cursor: "pointer", color: medium } }}
+                >Audio</Typography>
             </FlexBetween>
           </>
         ) : <FlexBetween gap="0.25rem">
